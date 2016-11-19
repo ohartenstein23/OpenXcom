5c8276d7173cf0ba56534a2d488554825447d1f2
ohartenstein23 <ohartenstein23@gmail.com>
2016-11-19 17:41:53 -0600

    Indicator for craft shields in basescape, weapon handling for bonus shields

--- src/Basescape/CraftInfoState.cpp
+++ src/Basescape/CraftInfoState.cpp
@@ -87,7 +87,15 @@ CraftInfoState::CraftInfoState(Base *base, size_t craftId) : _base(base), _craft
 	_btnArmor = new TextButton(64, 16, 16, bottom + 2 * bottom_row);
 	_btnPilots = new TextButton(64, 16, 16, bottom + 3 * bottom_row);
 	_edtCraft = new TextEdit(this, 140, 16, 80, 8);
-	_txtDamage = new Text(100, 17, 14, 24);
+	if (_craft->getShieldCapacity() != 0)
+	{
+		_txtDamage = new Text(100, 17, 14, 8);
+	}
+	else
+	{
+		_txtDamage = new Text(100, 17, 14, 24);
+	}
+	_txtShield = new Text(140, 17, 14, 24);
 	_txtFuel = new Text(82, 17, 228, 24);
 	for(int i = 0; i < _weaponNum; ++i)
 	{
@@ -122,6 +130,7 @@ CraftInfoState::CraftInfoState(Base *base, size_t craftId) : _base(base), _craft
 	add(_btnPilots, "button", "craftInfo");
 	add(_edtCraft, "text1", "craftInfo");
 	add(_txtDamage, "text1", "craftInfo");
+	add(_txtShield, "text1", "craftInfo");
 	add(_txtFuel, "text1", "craftInfo");
 	for(int i = 0; i < _weaponNum; ++i)
 	{
@@ -169,6 +178,9 @@ CraftInfoState::CraftInfoState(Base *base, size_t craftId) : _base(base), _craft
 	_txtDamage->setColor(Palette::blockOffset(13)+10);
 	_txtDamage->setSecondaryColor(Palette::blockOffset(13));
 
+	_txtShield->setColor(Palette::blockOffset(13)+10);
+	_txtShield->setSecondaryColor(Palette::blockOffset(13));
+
 	_txtFuel->setColor(Palette::blockOffset(13)+10);
 	_txtFuel->setSecondaryColor(Palette::blockOffset(13));
 
@@ -212,13 +224,30 @@ void CraftInfoState::init()
 	_txtDamage->setText(ss.str());
 
 	std::wostringstream ss2;
-	ss2 << tr("STR_FUEL").arg(Text::formatPercentage(_craft->getFuelPercentage()));
+	if (_craft->getShieldCapacity() != 0)
+	{
+		int shieldPercentage = std::floor((double)_craft->getShield() / _craft->getShieldCapacity() * 100);
+		ss2 << tr("STR_SHIELD").arg(Text::formatPercentage(shieldPercentage));
+		if (_craft->getShield() < _craft->getShieldCapacity())
+		{
+			int shieldHours = (int)ceil((double)_craft->getShieldCapacity() / _craft->getRules()->getShieldRechargeAtBase());
+			ss2 << formatTime(shieldHours);
+		}
+	}
+	else
+	{
+		ss2 << "";
+	}
+	_txtShield->setText(ss2.str());
+
+	std::wostringstream ss3;
+	ss3 << tr("STR_FUEL").arg(Text::formatPercentage(_craft->getFuelPercentage()));
 	if (_craft->getStatus() == "STR_REFUELLING" && _craft->getFuelMax() - _craft->getFuel() > 0)
 	{
 		int fuelHours = (int)ceil((double)(_craft->getFuelMax() - _craft->getFuel()) / _craft->getRules()->getRefuelRate() / 2.0);
-		ss2 << formatTime(fuelHours);
+		ss3 << formatTime(fuelHours);
 	}
-	_txtFuel->setText(ss2.str());
+	_txtFuel->setText(ss3.str());
 
 	if (_craft->getRules()->getSoldiers() > 0)
 	{
