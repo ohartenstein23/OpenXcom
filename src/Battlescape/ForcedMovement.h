950d2381d37cd206ec2f069240c871cbd30b6c02
ohartenstein23 <ohartenstein23@gmail.com>
2017-08-20 20:41:44 -0500

    Stating to create classes for forced movement.

---
+++ src/Battlescape/ForcedMovement.h
@@ -0,0 +1,44 @@
+#pragma once
+/*
+ * Copyright 2010-2017 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <vector>
+#include "Position.h"
+#include "BattlescapeGame.h"
+#include "../Mod/RuleItem.h"
+#include <SDL.h>
+
+namespace OpenXcom
+{
+
+class Tile
+//class TODO: list all the classes we need to know about for a ForceMovement
+
+/**
+ * This class represents the information necessary to do forced movement on a
+ * unit.
+ */
+class ForcedMovment
+{
+private:
+	//TODO put variables needed only for this class here
+public:
+	//TODO put functions that other classes need to know about here
+}
+
+}
---
+++ src/Battlescape/ForcedMovementBState.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010-2017 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ForcedMovementBState.h"
+#include "../Mod/RuleArmor.h"
+#include "../Mod/RuleItem.h"
+
+namespace OpenXcom
+{
+
+/**
+ * Sets up a forced movement battle state
+ * @param TODO list the parameters
+ */
+ForcedMovementBState::ForcedMovementBState(/*TODO necessary variables for init here*/) : /*TODO list of included variables with default values*/
+{
+	// TODO initialization commands here
+}
+
+/**
+ * Cleans up the forced movement battle state
+ */
+ForceMovementBState::~ForcedMovementBState()
+{
+
+}
+
+}
