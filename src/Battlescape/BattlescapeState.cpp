8590e8adf9054ea34d39180f93f92ae4db8835f4
ohartenstein23 <ohartenstein23@gmail.com>
2019-06-12 20:37:50 -0500

    Proof-of-concept editing tile data

--- src/Battlescape/BattlescapeState.cpp
+++ src/Battlescape/BattlescapeState.cpp
@@ -18,6 +18,7 @@
  */
 #include <algorithm>
 #include <sstream>
+#include <string> // TODO remove later
 #include <iomanip>
 #include <SDL_gfxPrimitives.h>
 #include "Map.h"
@@ -92,6 +93,7 @@ namespace OpenXcom
 /**
  * Initializes all the elements in the Battlescape screen.
  * @param game Pointer to the core game.
+ * @param editor Pointer to the data structure for the in-game map editor
  */
 BattlescapeState::BattlescapeState() :
 	_reserve(0), _manaBarVisible(false),
